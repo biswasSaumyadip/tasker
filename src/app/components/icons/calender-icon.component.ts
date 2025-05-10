a522edccfb4ef257036fad9ea2f80205540ac6e3
saumyadip biswas <saumyadip11@gmail.com>
2025-05-11 00:21:40 +0530

    feat: navbar styled

---
+++ src/app/components/icons/calender-icon.component.ts
@@ -0,0 +1,18 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'CalenderIcon', template: `
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor"
+         stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+         class="lucide lucide-calendar mr-3 flex-shrink-0 h-5 w-5 text-gray-400 group-hover:text-gray-500"
+         aria-hidden="true" data-id="element-62">
+      <path d="M8 2v4"></path>
+      <path d="M16 2v4"></path>
+      <rect width="18" height="18" x="3" y="4" rx="2"></rect>
+      <path d="M3 10h18"></path>
+    </svg>
+  `
+})
+export class CalenderIconComponent {
+
+}
--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import {RouterLink, RouterLinkActive} from '@angular/router';
 import {AvatarModule} from 'primeng/avatar';
+import {DashboardIconComponent} from '../icons/dashboard-icon.component';
+import {TasksIconComponent} from '../icons/tasks-icon.component';
+import {CalenderIconComponent} from '../icons/calender-icon.component'
+import {TeamIconComponent} from '../icons/team-icon.component';
+import {SettingsIconComponent} from '../icons/settings-icon.component';
 
 @Component({
   selector: 'tasker-sidebar',
-  imports: [AvatarModule, RouterLink, RouterLinkActive],
+  imports: [AvatarModule, RouterLink, RouterLinkActive, DashboardIconComponent, TasksIconComponent, CalenderIconComponent, TeamIconComponent, SettingsIconComponent],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
