bf8a3e7b1852830d7e95b14b4052b77592c77042
saumyadip biswas <saumyadip11@gmail.com>
2025-05-11 13:51:26 +0530

    feat: Styling and title bar for tasks page

--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -1,11 +1,9 @@
 import { Component } from '@angular/core';
-import {Button} from 'primeng/button';
+import { Button } from 'primeng/button';
 
 @Component({
 	selector: 'tasker-task-list',
-  imports: [
-    Button
-  ],
+	imports: [Button],
 	templateUrl: './task-list.component.html',
 	styleUrl: './task-list.component.scss',
 })
