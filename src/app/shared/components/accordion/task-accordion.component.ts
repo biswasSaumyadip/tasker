869cd60322c5dc3e356a6c84333ab1d4491886f8
saumyadip biswas <saumyadip11@gmail.com>
2025-05-15 11:03:58 +0530

    feat: Task accordion data handling

--- src/app/shared/components/accordion/task-accordion.component.ts
+++ src/app/shared/components/accordion/task-accordion.component.ts
@@ -1,4 +1,8 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input, InputSignal } from '@angular/core';
+import { Task } from '../../../models/task.model';
+import { DatePipe } from '@angular/common';
+import { CapitalizeFirstPipe } from '../../pipe/capitalize-first-pipe.pipe';
+import { PriorityBadgeDirective } from '../../directives/priority-badge.directive';
 
 @Component({
 	selector: 'tasker-accordion',
@@ -7,9 +11,14 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 	styleUrls: ['./task-accordion.component.scss'],
 	changeDetection: ChangeDetectionStrategy.OnPush,
 	imports: [
+		DatePipe,
+		CapitalizeFirstPipe,
+		PriorityBadgeDirective,
 		// Add any necessary imports here
 	],
 })
 export class TaskerAccordionComponent {
 	// Accordion component logic goes here
+	title: InputSignal<string> = input.required();
+	task: InputSignal<Task> = input.required();
 }
