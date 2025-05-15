a45cf37c9cc9ea46d17c4b39fa54a9c3a10401f1
saumyadip biswas <saumyadip11@gmail.com>
2025-05-15 11:06:13 +0530

    fix: Name changes for pipe test file

--- src/app/shared/pipe/capitalize-first-pipe.pipe.spec.ts
+++ src/app/shared/pipe/capitalize-first-pipe.pipe.spec.ts
@@ -1,8 +1,8 @@
-import { CapitalizeFirstPipePipe } from './capitalize-first-pipe.pipe';
+import { CapitalizeFirstPipe } from './capitalize-first-pipe.pipe';
 
-describe('CapitalizeFirstPipePipe', () => {
+describe('CapitalizeFirstPipe', () => {
 	it('create an instance', () => {
-		const pipe = new CapitalizeFirstPipePipe();
+		const pipe = new CapitalizeFirstPipe();
 		expect(pipe).toBeTruthy();
 	});
 });
