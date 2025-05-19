84c4a01dbef2c8f342253ab917ba041e8d747de1
saumyadip biswas <saumyadip11@gmail.com>
2025-05-19 22:55:07 +0530

    feat: Task creation page redirection added

--- src/app/components/task-list/task-list.component.spec.ts
+++ src/app/components/task-list/task-list.component.spec.ts
@@ -3,10 +3,12 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { TaskListComponent } from './task-list.component';
 import { provideHttpClientTesting } from '@angular/common/http/testing';
 import { provideHttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 describe('TaskListComponent', () => {
 	let component: TaskListComponent;
 	let fixture: ComponentFixture<TaskListComponent>;
+	let routerOutlet: Router;
 
 	beforeEach(async () => {
 		await TestBed.configureTestingModule({
@@ -17,9 +19,11 @@ describe('TaskListComponent', () => {
 		fixture = TestBed.createComponent(TaskListComponent);
 		component = fixture.componentInstance;
 		fixture.detectChanges();
+		routerOutlet = TestBed.inject(Router);
 	});
 
 	it('should create', () => {
 		expect(component).toBeTruthy();
+		expect(routerOutlet).toBeTruthy();
 	});
 });
--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { Button } from 'primeng/button';
 import { AnalyticsComponent } from './analytics/analytics.component';
 import { FilterComponent } from './filter/filter.component';
 import { TasksComponent } from './tasks/tasks.component';
+import { Router } from '@angular/router';
 
 @Component({
 	selector: 'tasker-task-list',
@@ -10,4 +11,10 @@ import { TasksComponent } from './tasks/tasks.component';
 	templateUrl: './task-list.component.html',
 	styleUrl: './task-list.component.scss',
 })
-export class TaskListComponent {}
+export class TaskListComponent {
+	private _router = inject(Router);
+
+	createTask() {
+		this._router.navigateByUrl('tasks/create');
+	}
+}
