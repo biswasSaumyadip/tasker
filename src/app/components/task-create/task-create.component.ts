0e018a392cb8d4a3091277c490959315d74801a8
saumyadip biswas <saumyadip11@gmail.com>
2025-05-23 13:55:09 +0530

    refactor: Accordion replaced with card for easy use of a dropdown list

--- src/app/components/task-create/task-create.component.ts
+++ src/app/components/task-create/task-create.component.ts
@@ -1,21 +1,25 @@
 import { Component } from '@angular/core';
 import { BackIconComponent } from '../icons/back-icon.component';
-import { Accordion, AccordionContent, AccordionHeader, AccordionPanel } from 'primeng/accordion';
 import { TaskerInputComponent } from '../../shared/components/tasker-input/tasker-input.component';
 import { RichTextEditorComponent } from '../../shared/components/rich-text-editor';
 import { FileUploadComponent } from '../../shared/components/file-upload/file-upload.component';
+import {
+	DropdownComponent,
+	DropdownOption,
+} from '../../shared/components/dropdown/dropdown.component';
+import { DatePicker } from 'primeng/datepicker';
+import { Card } from 'primeng/card';
 
 @Component({
 	selector: 'tasker-task-create',
 	imports: [
 		BackIconComponent,
-		Accordion,
-		AccordionPanel,
-		AccordionHeader,
-		AccordionContent,
 		TaskerInputComponent,
 		RichTextEditorComponent,
 		FileUploadComponent,
+		DropdownComponent,
+		DatePicker,
+		Card,
 	],
 	templateUrl: './task-create.component.html',
 	styleUrl: './task-create.component.scss',
@@ -23,5 +27,35 @@ import { FileUploadComponent } from '../../shared/components/file-upload/file-up
 export class TaskCreateComponent {
 	goBack() {
 		window.history.back();
+	}
+
+	priorities: DropdownOption<string>[] = [
+		{
+			label: 'All Priority',
+			value: 'all',
+		},
+		{
+			label: 'Low',
+			value: 'low',
+		},
+		{
+			label: 'Medium',
+			value: 'medium',
+		},
+		{
+			label: 'High',
+			value: 'high',
+		},
+		{
+			label: 'Urgent',
+			value: 'urgent',
+		},
+	];
+
+	priority = {};
+
+	onPrioritySelect(data: DropdownOption<string> | undefined) {
+		if (!data) return;
+		this.priority = data;
 	}
 }
